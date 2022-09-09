4ad911fc5ba4d58de0a90ae126fc17408ab960e8
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-09 01:28:34 +0100

    handle value out of ascii range

--- handle_pchar.c
+++ handle_pchar.c
@@ -9,9 +9,23 @@
 
 void handle_pchar(stack_t **stack, unsigned int line_number)
 {
+	char *save;
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	save = *stack;
+
+	while (save->next != NULL)
+		save = save->next;
+
+	if (save->n < 0 || save->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range", line_number);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
