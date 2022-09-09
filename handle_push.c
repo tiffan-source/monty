9dbbf8587c7aa52de794be5cbd7fde459a2d9c68
Tiffane <zohoungbogboanlyou@gmail.com>
2022-09-09 02:43:00 +0100

    handle negative number on push

--- handle_push.c
+++ handle_push.c
@@ -18,17 +18,18 @@ int _strlen(char *str)
 /**
  * check_number - check if string is a number
  * @str: string to check
+ * @index: index of char
  *
  * Return: 0 for success -1 else
  */
 
-int check_number(char *str)
+int check_number(char *str, int index)
 {
 	if (str == NULL)
 		return (-1);
 
-	if (*str >= '0' && *str <= '9')
-		return (check_number(str + 1));
+	if ((*str >= '0' && *str <= '9') || (*str == '-' && index == 0))
+		return (check_number(str + 1, index + 1));
 	else if (*str == '\0')
 		return (0);
 	else
@@ -48,7 +49,7 @@ void handle_push(stack_t **stack, unsigned int line)
 
 	test = strtok(NULL, " ");
 
-	if (test == NULL || check_number(test) == -1)
+	if (test == NULL || check_number(test, 0) == -1)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line);
 		free_stack(*stack);
